8598b6e2353b26aa44ce3a19a26078dc9cf50803
Blake Johnson <blake41@gmail.com>
2015-05-01 10:22:57 -0400

    updated to be non oo

--- spec/dice.spec.js
+++ spec/dice.spec.js
@@ -2,22 +2,17 @@
 
 describe('dice', function() {
 
-  var dice;
-  beforeEach(function() {
-    dice = new Dice();
-  });
-
   describe("Dice#roll", function(){
     it("should return a number", function() {
-      expect(typeof dice.roll()).toBe("number");
+      expect(typeof roll()).toBe("number");
     });      
 
     it("should return a number greater than 0", function() {
-      expect(dice.roll()).toBeGreaterThan(0);
+      expect(roll()).toBeGreaterThan(0);
     });  
 
     it("should return a number less than 7", function() {
-      expect(dice.roll()).toBeLessThan(7);
+      expect(roll()).toBeLessThan(7);
     });  
   });
 });
